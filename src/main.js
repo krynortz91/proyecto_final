38bc66b529508f372369620404a28b8f94e82286
GabiSh19 <gabs13.h@gmail.com>
2023-05-04 14:33:41 -0400

    se agrega bootstarp

--- src/main.js
+++ src/main.js
@@ -2,5 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// Import our custom CSS
+import './assets/styles.css'
 
-createApp(App).use(store).use(router).mount('#app')
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+createApp(App).use(bootstrap).use(store).use(router).mount('#app')
+
+
